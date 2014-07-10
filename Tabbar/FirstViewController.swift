b699ea35a33b0afe8efb8d3f85382965ad53e221
Philipp Süess <papsueess@n0003165.local>
2014-07-10 14:19:58 +0200

    Initial Commit

---
+++ Tabbar/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Tabbar
+//
+//  Created by Philipp Süess on 10.07.14.
+//  Copyright (c) 2014 Philipp Süess. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
