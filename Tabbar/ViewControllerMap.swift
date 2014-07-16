305b7ea1dda7af7c7494c7fe5070f0d8d0924c5e
Philipp Süess <papsueess@n0003165.local>
2014-07-16 14:18:54 +0200

    Added map location

--- Tabbar/ViewControllerMap.swift
+++ Tabbar/ViewControllerMap.swift
@@ -14,17 +14,42 @@ import MapKit
 
 class ViewControllerMap: UIViewController {
     
-    @IBOutlet var MApView: MKMapView
-    
-    
-    
+    @IBOutlet var MapView: MKMapView
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Längen- und Breitengrad der PH Luzern
+        var latitude:CLLocationDegrees =  47.0529200
+        var longitude:CLLocationDegrees = 8.3045690
+        
+        var latDelta:CLLocationDegrees = 0.01
+        var longDelta:CLLocationDegrees = 0.01
+        
+        var mapSpan:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, longDelta)
+        var PHLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        var mapRegion:MKCoordinateRegion = MKCoordinateRegionMake(PHLocation, mapSpan)
+        
+        self.MapView.setRegion(mapRegion, animated:true)
+        
+        var mapAnnotation = MKPointAnnotation()
+        mapAnnotation.coordinate = PHLocation
+        mapAnnotation.title = "Pädagogische Hochschule Luzern"
+        mapAnnotation.subtitle = "Löwengraben 14"
+        
+        // disable Annotation for userpin
+        self.MapView.userLocation.title = "You"
+        
+        // Add Annotation to map and select the Annotation by default
+        self.MapView.addAnnotation(mapAnnotation)
+        self.MapView.selectAnnotation(mapAnnotation, animated: true)
+        
     }
     
+
+
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
